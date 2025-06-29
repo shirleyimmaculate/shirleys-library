767e76935da8f837832030be8fd521fee7e7d5a5
shirleyimmaculate <mensahimmaculate@gmail.com>
2025-06-29 05:22:32 -0500

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Admin from './pages/Admin.jsx';
+
+export default function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<h1>Welcome to Shirley's Library</h1>} />
+        <Route path="/admin" element={<Admin />} />
+      </Routes>
+    </Router>
+  );
+}
