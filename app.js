0e342b0a2310dfc7131a460488b826a3377aaa1f
shirleyimmaculate <mensahimmaculate@gmail.com>
2025-06-29 05:12:22 -0500

    Update app.jsx

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
-// React app with routing using React Router
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Admin from './admin.jsx';
+
+export default function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/admin" element={<Admin />} />
+        <Route path="/" element={<div><h1>Welcome to Shirley's Library</h1></div>} />
+      </Routes>
+    </Router>
+  );
+}
