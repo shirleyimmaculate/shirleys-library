7bfccbecf1e700d68db1e394a13a0a4eb0345d8e
shirleyimmaculate <mensahimmaculate@gmail.com>
2025-06-29 05:11:25 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,2 +1,6 @@
-// React entry point: ReactDOM.render(...)
-import './app.js';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.jsx';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<App />);
