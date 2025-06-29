ddaa94d20efece0e0ce3c84849206b2593b5c865
shirleyimmaculate <mensahimmaculate@gmail.com>
2025-06-29 06:13:00 -0500

    Add files via upload

---
+++ src/pages/Admin.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function Admin() {
+  return (
+    <div>
+      <h2>Admin Portal</h2>
+      <p>This is a working Vite-powered React admin page! 🎉</p>
+    </div>
+  );
+}
