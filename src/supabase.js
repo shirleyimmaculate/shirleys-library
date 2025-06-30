16b1f45bc1fc56c9a346d580757a3d639886ea85
shirleyimmaculate <mensahimmaculate@gmail.com>
2025-06-30 18:22:39 -0500

    Delete src directory

--- src/supabase.js
+++
@@ -1,6 +0,0 @@
-import { createClient } from '@supabase/supabase-js'
-
-const supabaseUrl = 'https://kkisuqburdnmapbrgyfp.supabase.co'
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImtraXN1cWJ1cmRubWFwYnJneWZwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTA4OTczOTQsImV4cCI6MjA2NjQ3MzM5NH0.K2tx4vf9mT2SFZd6VbLPwVPw0zw6q3sCqnBePkKyuuI'
-
-export const supabase = createClient(supabaseUrl, supabaseKey)
