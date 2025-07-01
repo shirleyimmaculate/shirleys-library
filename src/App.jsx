b49a245338df224025cae7e8e55b6b5371f6c64d
shirleyimmaculate <mensahimmaculate@gmail.com>
2025-07-01 08:37:27 -0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Admin from './pages/Admin'
+import EditBook from './pages/EditBook'
 
 export default function App() {
   return (
@@ -8,6 +9,7 @@ export default function App() {
       <Routes>
         <Route path="/" element={<h1>Welcome to Shirley's Library</h1>} />
         <Route path="/admin" element={<Admin />} />
+        <Route path="/admin/edit" element={<EditBook />} />
       </Routes>
     </Router>
   )
